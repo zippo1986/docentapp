8e6781451b00652ea5932695adf6b6a0480ff75a
unknown <andresoktubre86@gmail.com>
2024-05-16 09:14:10 -0300

    Arrancando

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def hola_mundo(request,username):
+    
+    """
+    Vista que retorna un 'Hola Mundo' simple.
+    """
+    return HttpResponse("<h2>Hello %s </h2>" % username)
+
+def about(request):
+    """
+    Vista que retorna una página de 'Acerca de'.
+    """
+    return HttpResponse("Acerca de: Esta es una página de información sobre nues")
